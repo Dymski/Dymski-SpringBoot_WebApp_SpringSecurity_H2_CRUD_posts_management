1c248ee899ed3a25cab60cd6dc0418f6760d1cf6
Dymski <w.radymski@hotmail.com>
2020-05-16 11:58:32 +0200

    initial commit

---
+++ src/main/java/sda/twitterAtSDA/TwitterAtSdaApplication.java
@@ -0,0 +1,13 @@
+package sda.twitterAtSDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAtSdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterAtSdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sda/twitterAtSDA/TwitterAtSdaApplicationTests.java
@@ -0,0 +1,13 @@
+package sda.twitterAtSDA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterAtSdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
