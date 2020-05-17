7605bfe4609bee06c627c40037bfe10f2e7a7e41
Dymski <w.radymski@hotmail.com>
2020-05-17 10:02:16 +0200

    fix

--- src/main/java/sda/twitterAtSDA/configuration/SpringConfig.java
+++ src/main/java/sda/twitterAtSDA/configuration/SpringConfig.java
@@ -3,7 +3,6 @@ package sda.twitterAtSDA.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
