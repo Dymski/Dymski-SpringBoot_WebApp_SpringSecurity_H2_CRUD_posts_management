a7a7a158b152966820b956c1f831d8d4d9cfc128
krystianU <k.urbanik92@gmail.com>
2020-05-17 12:59:12 +0200

    UserController

--- src/main/java/sda/twitterAtSDA/controller/UserController.java
+++ src/main/java/sda/twitterAtSDA/controller/UserController.java
@@ -3,8 +3,15 @@ package sda.twitterAtSDA.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
+import sda.twitterAtSDA.model.dto.CreateUserDto;
+import sda.twitterAtSDA.model.entity.User;
 import sda.twitterAtSDA.service.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -12,7 +19,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
-
-
+    @PostMapping("/register")
+    public String addUser(@ModelAttribute CreateUserDto userDto){
+         userService.addUser(userDto);
+         return "redirect:/index";
+    }
 }
