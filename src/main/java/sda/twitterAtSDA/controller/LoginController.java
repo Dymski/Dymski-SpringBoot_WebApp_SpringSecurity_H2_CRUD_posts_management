bd21958dd8128e3680b051657b6c9abe18e3f646
Dymski <w.radymski@hotmail.com>
2020-05-17 09:57:26 +0200

    updates on login page

--- src/main/java/sda/twitterAtSDA/controller/LoginController.java
+++ src/main/java/sda/twitterAtSDA/controller/LoginController.java
@@ -2,6 +2,7 @@ package sda.twitterAtSDA.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -10,4 +11,9 @@ public class LoginController {
     public String loginView(){
         return "login";
     }
+
+//    @PostMapping("/login-process")
+
+
+
 }
