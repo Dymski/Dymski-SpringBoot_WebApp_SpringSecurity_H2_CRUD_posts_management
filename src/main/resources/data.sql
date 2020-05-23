abbaaae9a40bfef4680982c3da5ab83e525986a1
krystianU <k.urbanik92@gmail.com>
2020-05-23 12:04:34 +0200

    index html

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO USER(id, email, password, role) VALUES (1,'user', 'user', 'ROLE_USER');
 INSERT INTO USER(id, email, password, role) VALUES (2,'admin', 'admin', 'ROLE_ADMIN');
-INSERT INTO MESSAGE(messageID, postID, commentID, messageText) VALUES (1,1, 1, 'Przykładowy post');
+INSERT INTO MESSAGE(messageID, postID, commentID, message_text) VALUES (1,1, 1, 'Przykładowy post');
